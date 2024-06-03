fde4619cfa5c7be782fb0e97ea1ca91cff113ac2
caio1284 <00001090520694sp@al.educacao.sp.gov.br>
2024-06-03 12:09:54 -0300

    adicionando artigos js e css

---
+++ sketh.js
@@ -0,0 +1,17 @@
+function setup() {
+    background ("white");
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    stroke("red")
+    
+    fill("white")
+    
+    
+    // console.log(mouseIsPressed);
+    
+    if (mouseIsPressed);{
+    rect(mouseX, mouseY, 20, 35);
+    }
+  }
